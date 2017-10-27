de6914f379312833b125ad4160b4095a031037d6
Antoinette Janus <acj.gfx@gmail.com>
2017-10-27 16:34:15 -0400

    More heroku testing

--- db/server/zelda-server.js
+++ db/server/zelda-server.js
@@ -17,6 +17,9 @@ app.use(
 )
 app.use(cors())
 
+app.use('/', serveIndex(path.join(__dirname.replace('db/server', 'app'), '/index.html')))
+app.use('/', express.static(path.join(__dirname.replace('db/server', 'app'), '/index.html')))
+
 app.use('/public/sound_clips', serveIndex(path.join(__dirname, '/sound_clips')))
 app.use('/public/sound_clips', express.static(path.join(__dirname, '/sound_clips')))
 app.use('/public/user_songs', serveIndex(path.join(__dirname, '/user_songs')))
@@ -97,6 +100,6 @@ new CronJob('00 00 * * * *', function() {
   })
 }, null, true, 'America/New_York');
 
-const server = app.listen(3000, function() {
-  logger.log('info', 'Listening on port %d', server.address().port)
-})
+// const server = app.listen(3000, function() {
+//   logger.log('info', 'Listening on port %d', server.address().port)
+// })
