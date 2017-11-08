a4d2209bb1e33aee974b34204c08b23d46abeb96
Antoinette Janus <acj.gfx@gmail.com>
2017-11-08 10:30:17 -0500

    Added error state + sound

--- src/components/containers/email.js
+++ src/components/containers/email.js
@@ -18,43 +18,52 @@ const mapDispatchToProps = (dispatch, ownProps) => {
     hideEmail: ()=> {
       dispatch(toggleModal(false, 'email'))
       dispatch(setVisibleText('email-instruction'))
+      document.getElementById('error').classList.remove('active')
       document.getElementById('music-sheet').nextElementSibling.classList.remove('pending')
     },
     updateEmail: (email_address)=> {
       dispatch(setEmail(email_address))
       dispatch(toggleModal(true, 'email'))
+      document.getElementById('error').classList.remove('active')
     },
     backToInstructions: ()=> {
+      document.getElementById('error').classList.remove('active')
       document.querySelector('.black.button').classList.remove('pending')
       dispatch(setVisibleText('email-instructions'))
       dispatch(toggleModal(true, 'email'))
     },
     sendSong: (song, song_name, instrument, email_address)=> {
-      const formatted_song = song.map(function(note_obj){
-        return letters.filter(function(letter_obj){
-          if(letter_obj.letter === note_obj){
-            return letter_obj
-          }
-          return false
-        })[0].note
-      }).join('%20');
-      document.getElementById('submit-send-email').classList.add('pending')
-      fetch(`${base_url}/make-song`, {
-        method: 'post',
-        headers: {
-          'Accept': 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          notes: formatted_song,
-          instrument: instrument,
-          song_name: song_name,
-          email_address: email_address
+      if(email_address.length <= 0){
+        document.getElementById('error').classList.add('active')
+        document.getElementById('hey-listen').play()
+      } else {
+        document.getElementById('error').classList.remove('active')
+        const formatted_song = song.map(function(note_obj){
+          return letters.filter(function(letter_obj){
+            if(letter_obj.letter === note_obj){
+              return letter_obj
+            }
+            return false
+          })[0].note
+        }).join('%20');
+        document.getElementById('submit-send-email').classList.add('pending')
+        fetch(`${base_url}/make-song`, {
+          method: 'post',
+          headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            notes: formatted_song,
+            instrument: instrument,
+            song_name: song_name,
+            email_address: email_address
+          })
+        }).then(()=> {
+          dispatch(setVisibleText('email-sent'))
+          dispatch(toggleModal(true, 'email'))
         })
-      }).then(()=> {
-        dispatch(setVisibleText('email-sent'))
-        dispatch(toggleModal(true, 'email'))
-      })
+      }
     }
   }
 }
