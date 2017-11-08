960c38e2ebb418b194e0adc4236ec179743841f0
Antoinette Janus <acj.gfx@gmail.com>
2017-11-08 09:39:46 -0500

    Added test space

--- src/components/presenters/email.js
+++ src/components/presenters/email.js
@@ -9,7 +9,7 @@ const EmailPresenter = (props) => {
   if(props.visible_text === 'email-instructions'){
     email_modal_text = <div><h3>E-mail This Song</h3><div><p>Enter the e-mail where you would like the download URL for the song sent below.</p><p><label htmlFor="email_address">E-mail Address</label><input id="email_address" onChange={(e)=> { return props.updateEmail(e.target.value)}} /></p><button className="black button" onClick={()=>{ props.sendSong(props.notes, props.song_name, props.instrument, props.email_address) }}>Send Me My Song</button></div></div>
   } else {
-    email_modal_text = <div><h3>Song Sent</h3><div><p>Your song has been sent to {props.email_address}. If you would like to send this song to another e-mail address, <button className="button black" onClick={props.backToInstructions}>Click Here</button></p></div></div>
+    email_modal_text = <div><h3>Song Sent</h3><div><p>Your song has been sent to {props.email_address}. If you would like to send this song to another e-mail address, <button className="button black" onClick={props.backToInstructions}>Click Here</button> </p></div></div>
   }
   return (
     <div id="email-holder" className={classes}>
