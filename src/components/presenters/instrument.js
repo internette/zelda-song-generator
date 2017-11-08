bb20c57a8ae221ca969e30b902ab470c7b637c50
Antoinette Janus <acj.gfx@gmail.com>
2017-11-08 09:31:57 -0500

    Modified instrument image location

--- src/components/presenters/instrument.js
+++ src/components/presenters/instrument.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const InstrumentPresenter = (props) => {
-  const img_src = 'https://s3.amazonaws.com/zelda-song-generator/images/' + props.name + '.png'
+  const img_src = process.env.PUBLIC_URL + '/images/instruments/' + props.name + '.png'
   const alt = 'Click here to set the instrument to be ' + props.formatted_name
   const classes = props.active_instrument === props.name ? 'active instrument' : 'instrument'
   return (
